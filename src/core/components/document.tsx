7da8a44be7d7c472cd4a253808dbb93230d317ef
Thinh Tran <quangthinhtran3588@gmail.com>
2021-10-31 10:42:56 +0700

    update document

---
+++ src/core/components/document.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @next/next/no-document-import-in-page */
+import {SITE_NAME} from '@core/constants';
+import NextDocument, {Html, Head, Main, NextScript, DocumentInitialProps, DocumentContext} from 'next/document';
+
+export class Document extends NextDocument {
+  static getInitialProps = async (ctx: DocumentContext): Promise<DocumentInitialProps> => {
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    return {...initialProps};
+  };
+
+  render = (): JSX.Element => (
+    <Html>
+      <Head>
+        <meta name='application-name' content={SITE_NAME} />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content={SITE_NAME} />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='msapplication-config' content='/icons/browserconfig.xml' />
+        <meta name='msapplication-TileColor' content='#ffc40d' />
+        <meta name='msapplication-tap-highlight' content='no' />
+        <meta name='theme-color' content='#000000' />
+
+        <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
+
+        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+        <link rel='shortcut icon' href='/icons/favicon.ico' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:400,600,700&display=swap' />
+
+        {/* <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
+        <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
+        <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
+        <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
+        <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
+        <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
+        <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' /> */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
